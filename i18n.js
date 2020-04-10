5a3643aced42c193a1a661882a089c583f5b6bb9
000hen <42735043+000hen@users.noreply.github.com>
2020-04-10 18:53:31 +0800

    Add i18n System

---
+++ i18n.js
@@ -0,0 +1,41 @@
+function i18npc(cid,dcwr) {
+	var langid = cid;
+	
+	//check write
+	switch (dcwr) {
+		case true:
+			var docwrite = true;
+			break;
+		case false:
+			var docwrite = false;
+			break;
+		default:
+			var docwrite = true;
+	}
+	
+	//check user language
+	switch (app.getLocale()) {
+		case "en":
+			var langfile = "en.json";
+			break;
+		case "zh":
+			var langfile = "zh-TW.json";
+			break;
+		case "zh-CN":
+			var langfile = "zh-CN.json";
+			break;
+		case "zh-TW":
+			var langfile = "zh-TW.json";
+			break;
+		default:
+			var langfile = "en.json";		
+	}
+	
+	if (docwrite == false) {
+		var langpark = require("./lang/"+langfile);
+		return langpark[langid];
+	}else if (docwrite == true) {
+		var langpark = require("./lang/"+langfile);
+		document.write(langpark[langid]);
+	}
+}
